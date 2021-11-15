8cf7e01cc0e43886a3afb3bd30bb2ba5216f29db
Kalin Cormack <kalincormack@MacBook-Pro.local>
2021-11-15 13:51:09 -0500

    Cleaned up code

---
+++ colors.py
@@ -0,0 +1,4 @@
+'''
+Defines all the colors we'd like to use in the game
+'''
+
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ tutorial at: https://www.edureka.co/blog/snake-game-with-pygame/
 import pygame # game development framework
 import random # used to put food in random places on the screen
 from snake import Snake, snake
+from colors import blue, green, red, white
 
 # initialize the game
 pygame.init()
@@ -22,23 +23,11 @@ pygame.display.update()
 # set a caption on the screen
 pygame.display.set_caption('Snake game by Edureka and ISAT 252')
 
-# variables to hold different colors
-# TODO: remove unused colors
-blue = (0,0,255)
-red = (255,0,0)
-yellow = (255,255,0)
-white = (255,255,255)
-black = (0,0,0)
-green = (0,255,0)
-
 # Game clock: controls the speed of the game loop
 clock = pygame.time.Clock()
 snake_speed = 10 # higher number = faster snake = harder game
 snake_color = green
 snake_block = 20 
-
-# create a snake
-snake = Snake(green, dis.width / 2, dis.height / 2)
 
 # setup for messages to be displayed on the screen
 font_style = pygame.font.SysFont(None, 50)
@@ -48,10 +37,10 @@ def display_score(score):
     value = font_style.render("Your Score: " + str(score), True, blue)
     dis.blit(value, [10, 10])
 
-def draw_snake(segment_size, snake_list):
-    '''Draws the segments of the snake'''
-    for x in snake_list:
-        pygame.draw.rect(dis, snake_color, [x[0], x[1], segment_size, segment_size])
+# def draw_snake(segment_size, snake_list):
+#     '''Draws the segments of the snake'''
+#     for x in snake_list:
+#         pygame.draw.rect(dis, snake_color, [x[0], x[1], segment_size, segment_size])
 
 def message(msg,color):
     '''Displays a message 'msg' in a 'color' on the screen, anchored in the middle'''
@@ -65,22 +54,17 @@ def game_loop():
     game_over = False
     game_close = False  
 
-    # variables to control the x/y coordinates and size of the snake head
-    x1 = dis_width / 2
-    y1 = dis_height / 2  
-    x1_change = 0 
-    y1_change = 0
-    snake_list = []
-    snake_length = 1 
+    # create a snake
+    snake = Snake(green, dis_width / 2, dis_height / 2)
 
         # get random x/y coordinates for the food
     foodx = (
-            round(random.randrange(0, dis_width - snake_block) / snake_block)
-            * snake_block
+            round(random.randrange(0, dis_width - snake.head_size) / snake.head_size)
+            * snake.head_size
         )
     foody = (
-            round(random.randrange(0, dis_height - snake_block) / snake_block)
-            * snake_block
+            round(random.randrange(0, dis_height - snake.head_size) / snake.head_size)
+            * snake.head_size
         )
 
     # the main game loop. this loop will run infinitely until the value
@@ -114,40 +98,27 @@ def game_loop():
                     snake.move('down')
         
         # set 'game_close' to True if the snake goes outside of the screen boundary
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        if snake.x >= dis_width or snake.x < 0 or snake.y >= dis_height or snake.y < 0:
             game_over = True
         
-        x1 += x1_change
-        y1 += y1_change
+        snake.move()
         dis.fill(white)
 
         # draws red rectangle to represent the food
         pygame.draw.rect(dis, red, [foodx, foody, snake_block, snake_block])
 
-        # estalish the location of the snake's head
-        snake_head = []
-        snake_head.append(x1)
-        snake_head.append(y1)
-        snake_list.append(snake_head)
-
-        # makes sure we don't have a "phantom" or "extra" 
-        if len(snake.segments) > snake.length:
-                del snake.segments[0]
-        
         # chech to see if the snake's head intersects with any of the snake body parts
-        for x in snake_list[:-1]:
-                if x == snake_head:
-                    game_close = True
+        game_close = snake.has_crashed()
 
         snake.draw(dis)
-        draw_snake(snake_block, snake_list)
-        display_score(snake_length - 1)
+        # draw_snake(snake_block, snake_list)
+        display_score(snake.length - 1)
 
         # updates the surface (display area) with whatever changes
         # have been specified in this iteration through the game loop
         pygame.display.update()
 
-        if x1 == foodx and y1 == foody:
+        if snake.x == foodx and snake.y == foody:
             # get random x/y coordinates for the food
             foodx = (
                 round(random.randrange(0, dis_width - snake_block) / snake_block)
@@ -157,7 +128,7 @@ def game_loop():
                 round(random.randrange(0, dis_height - snake_block) / snake_block)
                 * snake_block
             )
-            snake_length += 1
+            snake.grow()
 
         # sets clock speed; higher number = faster game (and more difficult!)
         clock.tick(snake_speed)
